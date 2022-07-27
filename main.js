eff298fe005a26c727e9ecafe38ad66efd92701e
hetian <hetian@test.com>
2022-07-27 14:54:26 +0800

    完成首页开发

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
 import App from './App'
 
+//封装提示消息的方法
+uni.$showMsg = function(){
+	uni.showToast({
+		title:'数据请求失败!',
+		duration:1500,
+		icon:'none'
+	})
+}
+
+//引入第三方网络
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 响应拦截器
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
 Vue.config.productionTip = false
 
-App.mpType = 'app'
+App.mpType = 'app'	
 
 const app = new Vue({
 	...App
--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-	cate
+		hhh
 	</view>
 </template>
 
